ece45ed87901a8eb1003e4d03259d8db2fef2a6e
HaoHuynh0301 <hao152903@gmail.com>
2022-06-01 14:29:37 +0700

    refactor code.

--- api/controllers/resolverController.js
+++ api/controllers/resolverController.js
@@ -3,8 +3,10 @@
 const axios = require("axios").default;
 const { parseCookies, ensureAuthenticated } = require("../../core/index");
 const DID_CONTROLLER = "http://localhost:9000";
-const CARDANO_SERVICE = "http://http://18.139.84.180:10000";
-const AUTHENTICATION_SERVICE = "http://18.139.84.180:12000";
+// const CARDANO_SERVICE = "http://18.139.84.180:10000";
+const CARDANO_SERVICE = "http://192.168.1.22:10000";
+// const AUTHENTICATION_SERVICE = "http://18.139.84.180:12000";
+const AUTHENTICATION_SERVICE = "http://localhost:12000";
 
 /**
  * POST to create DID Doc for a DID
@@ -13,159 +15,173 @@ const AUTHENTICATION_SERVICE = "http://18.139.84.180:12000";
  * @returns {Object} DID Document of DID
  */
 exports.createDIDDocument = async function (req, res) {
-    const { did, didDocument } = req.body;
-    if (!did || !didDocument)
-        return res.status(400).send("Missing parameters.");
+  const { did, didDocument } = req.body;
+  if (!did || !didDocument) return res.status(400).send("Missing parameters.");
 
-    const didComponents = did.split(":");
-    if (didComponents.length < 4 || didComponents[0] != "did")
-        return res.status(400).json("Invalid DID syntax.");
+  const didComponents = did.split(":");
+  if (didComponents.length < 4 || didComponents[0] != "did")
+    return res.status(400).json("Invalid DID syntax.");
 
-    const companyName = didComponents[2];
-    const publicKey = didComponents[3];
-    await axios.post(DID_CONTROLLER + "/api/did/", {
-        companyName: companyName,
-        publicKey: publicKey,
-        content: didDocument
+  const companyName = didComponents[2];
+  const publicKey = didComponents[3];
+  await axios
+    .post(DID_CONTROLLER + "/api/did/", {
+      companyName: companyName,
+      publicKey: publicKey,
+      content: didDocument,
     })
     .then((response) => res.status(201).json(response.data))
     .catch((error) => res.status(400).json(error.response.data));
-}
-
+};
 
 /**
  * GET request to resolve DID
  * @param {String} did syntax is did:tradetrust:<companyName>:<documentName>
  * @returns {Object} DID Document of DID
  */
-exports.getDIDDocument = async function(req, res) {
-    const { did } = req.headers;
-    if (!did)
-        return res.status(400).send("Missing parameters.");
+exports.getDIDDocument = async function (req, res) {
+  const { did } = req.headers;
+  if (!did) return res.status(400).send("Missing parameters.");
 
-    const didComponents = did.split(":");
-    if (didComponents.length < 4 || didComponents[0] != "did")
-        return res.status(400).send("Invalid DID syntax.");
+  const didComponents = did.split(":");
+  if (didComponents.length < 4 || didComponents[0] != "did")
+    return res.status(400).send("Invalid DID syntax.");
 
-    const companyName = didComponents[2];
-    const fileName = didComponents[3];
-    await axios.get(DID_CONTROLLER + "/api/did/", {
-        headers: {
-            companyName: companyName,
-            fileName: fileName
-        }
+  const companyName = didComponents[2];
+  const fileName = didComponents[3];
+  await axios
+    .get(DID_CONTROLLER + "/api/did/", {
+      headers: {
+        companyName: companyName,
+        fileName: fileName,
+      },
     })
     .then((response) => res.status(200).json(response.data))
-    .catch((error) => (error.response) ? res.status(404).json(error.response.data) : res.status(400).json(error));
-}
+    .catch((error) =>
+      error.response
+        ? res.status(404).json(error.response.data)
+        : res.status(400).json(error)
+    );
+};
 
-exports.checkWrappedDocumentExistence = async function(req, res) {
-    const { fileName, companyName } = req.body;
-    console.log(req.body);
-    if (!fileName || !companyName) 
-        return res.status(400).send("Missing parameters.");
+exports.checkWrappedDocumentExistence = async function (req, res) {
+  const { fileName, companyName } = req.body;
+  console.log(req.body);
+  if (!fileName || !companyName)
+    return res.status(400).send("Missing parameters.");
 
-    await axios.get(DID_CONTROLLER + "/api/doc/exists/", {
-        headers: {
-            fileName, companyName
-        }
+  await axios
+    .get(DID_CONTROLLER + "/api/doc/exists/", {
+      headers: {
+        fileName,
+        companyName,
+      },
     })
     .then((existence) => res.status(200).json(existence.data.isExisted))
-    .catch((error) => (error.response) ? res.status(400).json(error.response.data) : res.status(400).json(error)); 
-}
+    .catch((error) =>
+      error.response
+        ? res.status(400).json(error.response.data)
+        : res.status(400).json(error)
+    );
+};
 
 /**
  * POST to creat wrapped document
  * @param {Object} wrappedDocument JSON object wrapped document, including did, hash and address.
  * @returns {JSON} message
  */
-exports.createWrappedDocument = async function(req, res) {
-    // console.log('REQUEST HEADER:\n', req.headers);
-    // const cookies = parseCookies(req);
-    // console.log(cookies)
-    // console.log('Cookie', cookies);
-    list = {};
-    var { access_token } = req.headers;
-    console.log("raw access_token", access_token);
-    // let [ name, ...rest] = access_token.split(`=`);
-    // name = name?.trim();
-    // if (!name) return;
-    // const value = rest.join(`=`).trim();
-    // if (!value) return;
-    // list[name] = decodeURIComponent(value);
-    // console.log("list", list);
-    // access_token = list.access_token;
-    // console.log("parsed access_token", access_token);
+exports.createWrappedDocument = async function (req, res) {
+  // console.log('REQUEST HEADER:\n', req.headers);
+  // const cookies = parseCookies(req);
+  // console.log(cookies)
+  // console.log('Cookie', cookies);
+  list = {};
+  var { access_token } = req.headers;
+  // let [ name, ...rest] = access_token.split(`=`);
+  // name = name?.trim();
+  // if (!name) return;
+  // const value = rest.join(`=`).trim();
+  // if (!value) return;
+  // list[name] = decodeURIComponent(value);
+  // console.log("list", list);
+  // access_token = list.access_token;
+  // console.log("parsed access_token", access_token);
 
-
- 
-    const { wrappedDocument } = req.body;
-    if (!wrappedDocument || !wrappedDocument.data.ddidDocument)
-        return res.status(400).send("Missing parameters.");
-    
+  const { wrappedDocument, address, did } = req.body;
+  if (!wrappedDocument || !wrappedDocument.data.ddidDocument) {
+     return res.status(400).send("Missing parameters."); 
+  }
     // const access_token = cookies.access_token,
-    const did = wrappedDocument.data.ddidDocument,
-        didComponents = did.split(":");
-    if (didComponents.length < 6) 
-        return res.status(400).send("Invalid DID syntax.");
+  const didComponents = did.split(":");
+  if (didComponents.length < 4) {
+    return res.status(400).send("Invalid DID syntax.");
+  }
+  const companyName = didComponents[2],
+    fileName = didComponents[3],
+    issuerAddress = wrappedDocument.data.issuers[0].address,
+    targetHash = wrappedDocument.signature.targetHash;
 
-    const companyName = didComponents[4], 
-        fileName = didComponents[5],
-        issuerAddress = wrappedDocument.data.issuers[0].address,
-        targetHash = wrappedDocument.signature.targetHash;
+  try {
+    // const address = await axios.get(AUTHENTICATION_SERVICE + "/api/auth/verify", {
+    //     withCredentials: true,
+    //     headers: {
+    //         "Cookie": `access_token=${access_token};`,
+    //     }
+    // });
+    const address = await axios.get(
+      "http://192.168.1.26:12000" + "/api/auth/verify",
+      {
+        withCredentials: true,
+        headers: {
+          Cookie: `${access_token};`,
+        },
+      }
+    );
+    // if (address !== issuerAddress)
+    //     return res.status(401).send("Unauthorized");
 
-    try {
-        // const address = await axios.get(AUTHENTICATION_SERVICE + "/api/auth/verify", {
-        //     withCredentials: true,
-        //     headers: {
-        //         "Cookie": `access_token=${access_token};`,
-        //     }
-        // });
-        const address = await axios.get("http://192.168.1.26:12000" + "/api/auth/verify", {
-            withCredentials: true,
-            headers: {
-                "Cookie": `access_token=${access_token};`,
-            }
-        });
-        console.log("CALL A TAM (HAO): ", address.data.data.address);
-
-        // if (address !== issuerAddress) 
-        //     return res.status(401).send("Unauthorized");
-        
-        const existence = await axios.get(DID_CONTROLLER + "/api/doc/exists/", {
-            headers: {
-                companyName, 
-                fileName
-            }
-        });
-        if (existence.data.isExisted) {
-            return res.status(400).send("File name exsited");
-        }
-        
-        console.log("CALLING KHANG");
-        const storingHash = await axios.put(CARDANO_SERVICE + "/api/storeHash/", {
-            withCredentials: true,
-            headers: {
-                "Cookie": `access_token=${access_token};`,
-            },
-            body: {
-                address: address.data.data.address,
-                hash: targetHash
-            }
-        });
-        const storingHashStatus = storingHash.data.result;
-        if (storingHashStatus !== "true") {
-            return res.status(400).send(storingHashStatus, ". Cannot store hash.");
-        }
-        
-        const storingWrappedDocumentStatus = await axios.post(DID_CONTROLLER = "/api/docs", {
-            fileName, wrappedDocument, companyName    
-        });
-        console.log(storingWrappedDocumentStatus.data);
-        return res.status(200).json(storingWrappedDocumentStatus.data);
+    const existence = await axios.get(DID_CONTROLLER + "/api/doc/exists/", {
+      headers: {
+        companyName,
+        fileName,
+      },
+    });
+    if (existence.data.isExisted) {
+      return res.status(400).send("File name exsited");
     }
-    catch (err) {
-        console.log(err);
-        return (err.response) ? res.status(400).json(err.response.data) : res.status(400).json(err);
+    const storingHash = await axios.put(CARDANO_SERVICE + "/api/storeHash/", {
+      withCredentials: true,
+      headers: {
+        Cookie: `${access_token};`,
+      },
+      body: {
+        address: address.data.data.address,
+        hash: targetHash,
+      },
+    });
+    if(storingHash.error_code) {
+        return res.status(400).send('Loi rui')
     }
-}
+    const storingHashStatus = storingHash.data.result;
+    console.log(storingHashStatus);
+    if (storingHashStatus !== "true") {
+      return res.status(400).send(storingHashStatus, ". Cannot store hash.");
+    }
+
+    const storingWrappedDocumentStatus = await axios.post(
+      (DID_CONTROLLER = "/api/docs"),
+      {
+        fileName,
+        wrappedDocument,
+        companyName,
+      }
+    );
+    console.log(storingWrappedDocumentStatus.data);
+    return res.status(200).json(storingWrappedDocumentStatus.data);
+  } catch (err) {
+    // console.log(err);
+    return err.response
+      ? res.status(400).json(err.response.data)
+      : res.status(400).json(err);
+  }
+};
