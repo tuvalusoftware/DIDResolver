2f49a9112ff642636bd22b634b60ce6980e82b93
Gia Buỳnh <giabinh0899@gmail.com>
2022-06-03 15:15:05 +0700

    Fix: swagger

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const http = require("http");
-const express = require("express");
-const cors = require("cors");
-const port = process.env.PORT || 8000;
-const bodyParser = require("body-parser");
-const compression = require('compression');
-const methodOverride = require('method-override');
+const http = require("http"),
+  express = require("express"),
+  cors = require("cors"),
+  bodyParser = require("body-parser"),
+  compression = require("compression"),
+  methodOverride = require("method-override");
+swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("./swagger/"),
+  port = process.env.PORT || 8000;
+
 const app = express();
+
 // routes
 const routes = require("./api/routes/resolverRoutes");
 routes(app);
@@ -14,14 +18,13 @@ app.use(cors());
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 app.use(methodOverride());
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const server = http.createServer(app);
 
@@ -33,5 +36,5 @@ app.use((err, res) => {
   });
 });
 server.listen(port, () => {
-  console.log(`Listening on http://localhost${port}`);
+  console.log(`Listening on http://localhost:${port}`);
 });
