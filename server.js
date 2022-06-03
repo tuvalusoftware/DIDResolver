62563827fb96f6a1735d2d85756dfafabb7ec4ac
Gia Buỳnh <giabinh0899@gmail.com>
2022-06-03 14:13:49 +0700

    Fix conflict while using CORS

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
-var express = require("express"),
-  cors = require("cors"),
-  app = express(),
-  port = process.env.PORT || 8000,
-  bodyParser = require("body-parser");
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 8000;
+const bodyParser = require("body-parser");
+const compression = require('compression');
+const methodOverride = require('method-override');
+const app = express();
+// routes
+const routes = require("./api/routes/resolverRoutes");
+routes(app);
 
-var swaggerUi = require("swagger-ui-express"),
-  swaggerDocument = require("./swagger/");
-
+app.use(cors());
+app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(methodOverride());
 
-// routes
-var routes = require("./api/routes/resolverRoutes");
-routes(app);
 
-app.listen(port);
+const server = http.createServer(app);
 
-console.log("DID Resolver RESTfull API server start on: " + port);
-app.use(function(req, res) {
-    res.status(404).send({url: req.originalUrl + " not found"});
+app.use((err, res) => {
+  res.json({
+    error_code: err.error_code || err.message,
+    error_message: err.message,
+    error_data: err.error_data,
+  });
+});
+server.listen(port, () => {
+  console.log(`Listening on http://localhost${port}`);
 });
