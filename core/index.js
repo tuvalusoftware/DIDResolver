879ef1f14e65ce0145e4809e49e4ae86f831c43c
Gia Buỳnh <giabinh0899@gmail.com>
2022-06-16 17:55:06 +0700

    Add new endpoint update wrapped document --pending

--- core/index.js
+++ core/index.js
@@ -1,4 +1,5 @@
 const cardanoSerialization = require("@emurgo/cardano-serialization-lib-nodejs");
+const Ajv = require("ajv");
 
 module.exports.ensureAuthenticated = (req, res, next) => {
   if (!req.cookies["access_token"]) return res.sendStatus(401);
@@ -30,3 +31,11 @@ module.exports.ensureAuthenticated = (req, res, next) => {
 module.exports.getAddressFromHexEncoded = (hexAddress) => {
   return cardanoSerialization.Address.from_bytes(Buffer.from(hexAddress, 'hex')).to_bech32();
 };
+
+module.exports.validatJSONSchema = (schema, object) => {
+  const ajv = new Ajv();
+  const validate = ajv.compile(schema);
+  const valid = validate(object);
+  console.log(valid);
+  return valid ? true : false;
+}
