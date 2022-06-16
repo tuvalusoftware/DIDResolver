995a00490c75536f5171e33b3c31ef62f6fd6547
Gia Buỳnh <giabinh0899@gmail.com>
2022-06-16 13:25:02 +0700

    Refactor document endpoint

--- api/routes/documentRoutes.js
+++ api/routes/documentRoutes.js
@@ -5,6 +5,14 @@ const router = express.Router();
 //  router.use(authControler.ensureAuthenticated);
 
 router.get("/document", documentController.getDocuments);
+
+router.get("/did-document", documentController.getDIDDocument);
 router.post("/did-document", documentController.createDIDDocument);
+
+router.post("/wrapped-document", documentController.createWrappedDocument);
+router.get("/wrapped-document/valid", documentController.validateWrappedDocument);
+router.get("/wrapped-document/exist", documentController.checkWrappedDocumentExistence);
+
+
 
 module.exports = router;
