64b99693bcf24a96c3badf559067373ae9b1b415
Gia Buỳnh <giabinh0899@gmail.com>
2022-07-02 14:59:27 +0700

    Add middleware

--- api/routes/credentialRoutes.js
+++ api/routes/credentialRoutes.js
@@ -3,7 +3,7 @@ const credentialController = require("../controllers/credentialController");
 const authController = require("../controllers/authController");
 const router = express.Router();
 
-// router.use(authController.ensureAuthenticated);
+router.use(authController.ensureAuthenticated);
 
 router.post("/credential", credentialController.createCredential);
 
