552480107ff9f6552b43f104f837083284c078c6
Gia Buỳnh <giabinh0899@gmail.com>
2022-08-05 18:22:39 +0700

    Change host to https

--- core/constants.js
+++ core/constants.js
@@ -3,14 +3,8 @@ const LOCAL_HOST = "http://localhost";
 const SCHEMAS = require("./schemas/index");
 
 module.exports.SERVERS = {
-<<<<<<< HEAD
-  DID_CONTROLLER: `${HOST}:9000`,
-  // CARDANO_SERVICE: `http://192.168.2.3:10003`,
-  CARDANO_SERVICE: `${HOST}:10003`,
-=======
   // CARDANO_SERVICE: `${HOST}:10003`,
   // DID_CONTROLLER: `${HOST}:9000`,
->>>>>>> temp
   AUTHENTICATION_SERVICE: `${HOST}:12000`,
   CARDANO_SERVICE: `https://e19c-18-139-84-180.ap.ngrok.io`,
   DID_CONTROLLER: `https://d3db-18-139-84-180.ap.ngrok.io`,
