d6dd3c0530358a704740b420cff56a261511c396
Gia Buỳnh <giabinh0899@gmail.com>
2022-08-12 11:41:45 +0700

    Config server to load image

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(cors());
 app.use(cookieParser());
 app.use(compression());
 app.use(express.json());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 app.use((err, req, res, _next) => {
   res.json({
     error_message: "Body should be a JSON",
@@ -25,15 +27,19 @@ app.use(
 );
 app.use(methodOverride());
 
-// SET UP SWAGGER API DOCUMENT 
+// SET UP SWAGGER API DOCUMENT
 const swaggerUi = require("swagger-ui-express"),
   swaggerDocument = require("./swagger/");
 var swaggerOptions = {
-  customCss: '.swagger-ui .topbar { display: none }',
+  customCss: ".swagger-ui .topbar { display: none }",
   customSiteTitle: "DID Resolver API",
   // customfavIcon: "/assets/favicon.ico"
 };
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, swaggerOptions)
+);
 
 // ROUTE
 const server = http.createServer(app);
