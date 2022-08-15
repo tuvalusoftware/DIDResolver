6f6b7a77e812dd8797dab1b071a9c5cc108b3f7f
Gia Buỳnh <giabinh0899@gmail.com>
2022-08-15 15:12:54 +0700

    Configure CORS

--- server.js
+++ server.js
@@ -26,6 +26,26 @@ app.use(
 );
 app.use(methodOverride());
 app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+// const corsOptions = {
+//   origin: ["http://localhost:3000"],
+//  //update: or "origin: true," if you don't wanna add a specific one
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
 
 // SET UP SWAGGER API DOCUMENT
 const swaggerUi = require("swagger-ui-express"),
