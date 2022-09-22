24cda9e20c3449b5750f25e050daa834eed4cbd0
ngoquocbao1010 <totti.chelsea@gmail.com>
2022-09-22 16:41:53 +0700

    📝 update: refactor

--- api/controllers/cardanoController.js
+++ api/controllers/cardanoController.js
@@ -6,168 +6,171 @@ const { apiInfo, apiError, info, error } = require("../../logger");
 axios.defaults.withCredentials = true;
 
 module.exports = {
-  getNFTs: async function (req, res) {
-    const { access_token } = req.cookies;
-    const { policyid: policyId } = req.headers;
+    getNFTs: async function (req, res) {
+        const { access_token } = req.cookies;
+        const { policyid: policyId } = req.headers;
 
-    try {
-      // Handle input errors
-      const undefinedVar = checkUndefinedVar({ policyId });
-      if (undefinedVar.undefined)
-        return res.status(200).json({
-          ...ERRORS.MISSING_PARAMETERS,
-          detail: undefinedVar.detail,
-        });
+        try {
+            // Handle input errors
+            const undefinedVar = checkUndefinedVar({ policyId });
+            if (undefinedVar.undefined)
+                return res.status(200).json({
+                    ...ERRORS.MISSING_PARAMETERS,
+                    detail: undefinedVar.detail,
+                });
 
-      // Call Cardano Service
-      // success:
-      // v2
-      //   {
-      //     code: 0,
-      //     message: string,
-      //     data: [] or {}
-      //   }
-      // error:
-      //   { code: 1, message: string }
-      const { data } = await axios.post(
-        `${SERVERS.CARDANO_SERVICE}/api/v2/fetch/nft`,
-        {
-          // (opt) asset: 41ac0d6b1287d7abd2c0443566cf34f95f1ec7e338dc18653fcedcb4ec9df0f3318725fa3c636ee118d233cadf8bd4a6f654c89f164d1169cc473cdc
-          policyId: policyId, // 41ac0d6b1287d7abd2c0443566cf34f95f1ec7e338dc18653fcedcb4
-        },
-        {
-          withCredentials: true,
-          headers: {
-            Cookie: `access_token=${access_token};`,
-          },
+            // Call Cardano Service
+            // success:
+            // v2
+            //   {
+            //     code: 0,
+            //     message: string,
+            //     data: [] or {}
+            //   }
+            // error:
+            //   { code: 1, message: string }
+            const { data } = await axios.post(
+                `${SERVERS.CARDANO_SERVICE}/api/v2/fetch/nft`,
+                {
+                    // (opt) asset: 41ac0d6b1287d7abd2c0443566cf34f95f1ec7e338dc18653fcedcb4ec9df0f3318725fa3c636ee118d233cadf8bd4a6f654c89f164d1169cc473cdc
+                    policyId: policyId, // 41ac0d6b1287d7abd2c0443566cf34f95f1ec7e338dc18653fcedcb4
+                },
+                {
+                    withCredentials: true,
+                    headers: {
+                        Cookie: `access_token=${access_token};`,
+                    },
+                }
+            );
+
+            if (data.code) {
+                apiError(req, res, `${JSON.stringify(data)}`);
+                return res.status.json(ERRORS.CANNOT_FETCH_NFT);
+            } else {
+                apiInfo(req, res, `Success.\n${JSON.stringify(data)}`);
+                return res.status(200).json(data.data);
+            }
+        } catch (error) {
+            apiInfo(req, res, `${JSON.stringify(error)}`);
+            return error.response
+                ? res.status(400).json(error.response.data)
+                : res.status(400).json(error);
         }
-      );
+    },
 
-      if (data.code) {
-        apiError(req, res, `${JSON.stringify(data)}`);
-        return res.status.json(ERRORS.CANNOT_FETCH_NFT);
-      } else {
-        apiInfo(req, res, `Success.\n${JSON.stringify(data)}`);
-        return res.status(200).json(data.data);
-      }
-    } catch (error) {
-      apiInfo(req, res, `${JSON.stringify(error)}`);
-      return error.response
-        ? res.status(400).json(error.response.data)
-        : res.status(400).json(error);
-    }
-  },
+    verifyHash: async function (req, res) {
+        // Receive input data
+        const { access_token } = req.cookies;
+        const { hashofdocument, policyid } = req.headers;
 
-  verifyHash: async function (req, res) {
-    // Receive input data
-    const { access_token } = req.cookies;
-    const { hashofdocument, policyid } = req.headers;
+        try {
+            // Handle input errors
+            const undefinedVar = checkUndefinedVar({
+                hashofdocument,
+                policyid,
+            });
+            if (undefinedVar.undefined)
+                return res.status(200).json({
+                    ...ERRORS.MISSING_PARAMETERS,
+                    detail: undefinedVar.detail,
+                });
 
-    try {
-      // Handle input errors
-      const undefinedVar = checkUndefinedVar({ hashofdocument, policyid });
-      if (undefinedVar.undefined)
-        return res.status(200).json({
-          ...ERRORS.MISSING_PARAMETERS,
-          detail: undefinedVar.detail,
-        });
+            // Call Cardano Service
+            // v2
+            // success:
+            //   {
+            //     code: 0,
+            //     message: string,
+            //     data: [] or {}
+            //   }
+            // error:
+            //   { code: 1, message: string }
+            let query = { policyId: policyid },
+                undefinedCheck = { policyid };
+            if (hashofdocument) {
+                query = { asset: `${policyid}${hashofdocument}` };
+                undefinedCheck = { ...undefinedCheck, hashofdocument };
+            }
 
-      // Call Cardano Service
-      // v2
-      // success:
-      //   {
-      //     code: 0,
-      //     message: string,
-      //     data: [] or {}
-      //   }
-      // error:
-      //   { code: 1, message: string }
-      let query = { policyId: policyid },
-        undefinedCheck = { policyid };
-      if (hashofdocument) {
-        query = { asset: `${policyid}${hashofdocument}` };
-        undefinedCheck = { ...undefinedCheck, hashofdocument };
-      }
+            const { data } = await axios.post(
+                `${SERVERS.CARDANO_SERVICE}/api/v2/fetch/nft`,
+                query,
+                {
+                    withCredentials: true,
+                    headers: {
+                        Cookie: `access_token=${access_token}`,
+                    },
+                }
+            );
 
-      const { data } = await axios.post(
-        `${SERVERS.CARDANO_SERVICE}/api/v2/fetch/nft`,
-        query,
-        {
-          withCredentials: true,
-          headers: {
-            Cookie: `access_token=${access_token}`,
-          },
+            if (data.code) {
+                apiError(req, res, `${JSON.stringify(data)}`);
+                return res.status.json(ERRORS.CANNOT_FETCH_NFT);
+            } else {
+                apiInfo(req, res, `Success.\n${JSON.stringify(data)}`);
+                return res.status(200).json(data.data);
+            }
+        } catch (error) {
+            apiError(req, res, error);
+            return error.response
+                ? res.status(400).json(error.response.data)
+                : res.status(400).json(error);
         }
-      );
+    },
 
-      if (data.code) {
-        apiError(req, res, `${JSON.stringify(data)}`);
-        return res.status.json(ERRORS.CANNOT_FETCH_NFT);
-      } else {
-        apiInfo(req, res, `Success.\n${JSON.stringify(data)}`);
-        return res.status(200).json(data.data);
-      }
-    } catch (error) {
-      apiError(req, res, error);
-      return error.response
-        ? res.status(400).json(error.response.data)
-        : res.status(400).json(error);
-    }
-  },
+    verifySignature: async function (req, res) {
+        // Receive input data
+        const { access_token } = req.cookies;
+        const { address, payload, signature, key } = req.headers;
 
-  verifySignature: async function (req, res) {
-    // Receive input data
-    const { access_token } = req.cookies;
-    const { address, payload, signature, key } = req.headers;
+        try {
+            // Handle input error
+            const undefinedVar = checkUndefinedVar({
+                address,
+                payload,
+                signature,
+                key,
+            });
+            if (undefinedVar.undefined)
+                return res.status(200).json({
+                    ...ERRORS.MISSING_PARAMETERS,
+                    detail: undefinedVar.detail,
+                });
 
-    try {
-      // Handle input error
-      const undefinedVar = checkUndefinedVar({
-        address,
-        payload,
-        signature,
-        key,
-      });
-      if (undefinedVar.undefined)
-        return res.status(200).json({
-          ...ERRORS.MISSING_PARAMETERS,
-          detail: undefinedVar.detail,
-        });
+            // Call Cardano Service
+            // success:
+            //   {
+            //     code: number,
+            //     message: string,
+            //     data: true/false
+            //   }
+            // error:
+            //   { code: number, message: string }
+            const { data } = await axios.post(
+                SERVERS.CARDANO_SERVICE + "/api/v2/verify/signature",
+                {
+                    address: address,
+                    payload: payload,
+                    signature: signature,
+                    key: key,
+                },
+                {
+                    withCredentials: true,
+                    headers: {
+                        Cookie: `access_token=${access_token}`,
+                    },
+                }
+            );
 
-      // Call Cardano Service
-      // success:
-      //   {
-      //     code: number,
-      //     message: string,
-      //     data: true/false
-      //   }
-      // error:
-      //   { code: number, message: string }
-      const { data } = await axios.post(
-        SERVERS.CARDANO_SERVICE + "/api/v2/verify/signature",
-        {
-          address: address,
-          payload: payload,
-          signature: signature,
-          key: key,
-        },
-        {
-          withCredentials: true,
-          headers: {
-            Cookie: `access_token=${access_token}`,
-          },
+            data.code
+                ? apiError(req, res, `${JSON.stringify(data)}`)
+                : apiInfo(req, res, `Success.\n${JSON.stringify(data)}`);
+            return res.status(200).json(data.data);
+        } catch (error) {
+            apiError(req, res, `${JSON.stringify(error)}`);
+            return error.response
+                ? res.status(400).json(error.response.data)
+                : res.status(400).json(error);
         }
-      );
-
-      data.code
-        ? apiError(req, res, `${JSON.stringify(data)}`)
-        : apiInfo(req, res, `Success.\n${JSON.stringify(data)}`);
-      return res.status(200).json(data.data);
-    } catch (error) {
-      apiError(req, res, `${JSON.stringify(error)}`);
-      return error.response
-        ? res.status(400).json(error.response.data)
-        : res.status(400).json(error);
-    }
-  },
+    },
 };
